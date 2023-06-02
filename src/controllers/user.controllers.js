dc2bd3a81623cc03caab2bce746e69751d89db14
vicdan7 <ibarravictor334@gmail.com>
2023-06-02 16:02:16 -0500

    crud backend

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,45 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const users = await User.findAll()
+    return res.json(users)
+});
+
+const create = catchError(async(req, res) => {
+    const user = req.body
+    const createUser = await User.create(user)
+    return res.status(201).json(createUser)
+})
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params
+    const oneUser = await User.findByPk(id)
+    if(!oneUser) return res.status(404).json({message: "user not found"})
+    return res.json(oneUser)
+})
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params
+    const removeUser = await User.destroy({where: { id }})
+    if(!removeUser) return res.status(404).json({message: "user not found"})
+    return res.sendStatus(204)
+})
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params
+    const userBody = req.body
+    const userUpdate = await User.update(userBody, {where:{ id }, returning:true})
+    if (userUpdate[0]=0) return res.status(404).json({message: "user not found"})
+    
+    return res.json(userUpdate[1][0])
+})
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+
+}
---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+
+const User = sequelize.define('user', {
+    
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    birthday: {
+        type: DataTypes.STRING,
+    },
+   
+});
+
+module.exports = User;
